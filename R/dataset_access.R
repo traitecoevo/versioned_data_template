9ad746f601150628d9847481dee035a6505768a5
unknown <fabricesamonte@outlook.com>
2019-12-09 14:25:55 +1100

    1.0.0 release

--- R/dataset_access.R
+++ R/dataset_access.R
@@ -36,8 +36,8 @@ dataset_access_function <- function(version=NULL, path=NULL) {
 ##   3. the function to read the file, given a filename (read_csv)
 dataset_info <- function(path) {
   datastorr::github_release_info("FabriceSamonte/datastorrtest",
-                                 filename=c("Globcover_Legend.xls"),
-                                 read=c(read_spreadsheet),
+                                 filename=c("baad_with_map.csv"),
+                                 read=c(read_csv),
                                  path=path)
 }
 
